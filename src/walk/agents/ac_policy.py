d12f814d8c4763021f995ba595232f538e4b2a6a
AlEmerich <alan.guitard.pro@gmail.com>
2018-04-20 19:48:45 +0200

    first shot of train critic

--- src/walk/agents/ac_policy.py
+++ src/walk/agents/ac_policy.py
@@ -2,6 +2,7 @@ from .abstract_env import AbstractHumanoidEnv
 from ..models.actor_critic import ActorCritic
 from ..utils.memory import Memory
 import numpy as np
+import tensorflow as tf
 
 class AC_Policy(AbstractHumanoidEnv):
 
@@ -16,24 +17,28 @@ class AC_Policy(AbstractHumanoidEnv):
         self.target_critic_model = ActorCritic.critic_model(self.env)
 
     def _train_actor(self, state, action, reward, new_state, done):
-        pass
+        for sample in samples:
+            state, action, reward, new_state, done = sample
+
+            
 
     def _train_critic(self, samples):
         for sample in samples:
             state, action, reward, new_state, done = sample
-            
+
             # Get the Q-value of the action given the state
             Q_value = self.critic_model.predict([state, action])
 
             # Get the action the actor will take with new state
             next_action = self.target_actor_model(new_state)
 
-            # Get the Q_+1-value 
-            Q_value_p1 = self.target_critic_model([new_state, new_action])
+            # Get the Q_+1-value
+            Q_value_p1 = self.target_critic_model([new_state, next_action])
 
             # Don't see in the future on action which lead to done
             future_rewards = reward + self.params.gamma * Q_value_p1 * (1 - done)
 
+            self.critic_model.fit([state, action], future_rewards)
 
 
     def train(self):
@@ -46,8 +51,8 @@ class AC_Policy(AbstractHumanoidEnv):
 
         # Let's try first to run samples on critic then on actor
         # And after one sample per sample
-        _train_critic(samples)
-        _train_actor(samples)
+        self._train_critic(samples)
+        self._train_actor(samples)
 
     def act(self, state):
         epsilon = self.params.epsilon
