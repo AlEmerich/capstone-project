946e7a56f25b7d8f6f0169171e278a30eace61ba
AlEmerich <alan@projet-lucine.org>
2018-06-01 17:10:09 +0200

    update report

--- report/auto/report.el
+++ report/auto/report.el
@@ -3,12 +3,12 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babel" "english") ("natbib" "square" "comma" "sort" "numbers") ("fontenc" "T1")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -21,9 +21,9 @@
     "graphicx"
     "xcolor"
     "amsmath"
+    "amssymb"
     "multicol"
     "fontenc")
-   (LaTeX-add-bibliographies
-    "biblio"))
+   (LaTeX-add-bibliographies))
  :latex)
 
