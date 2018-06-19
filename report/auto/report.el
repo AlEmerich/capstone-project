819b54c9a2488fe112151a66b00c3169db4dfda2
AlEmerich <alan@projet-lucine.org>
2018-06-19 14:20:29 +0200

    finish until Refinement section

--- report/auto/report.el
+++ report/auto/report.el
@@ -4,15 +4,15 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babel" "english") ("natbib" "square" "comma" "sort" "numbers") ("ragged2e" "document") ("fontenc" "T1") ("caption" "font=scriptsize")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -27,6 +27,7 @@
     "ragged2e"
     "amssymb"
     "listings"
+    "adjustbox"
     "multicol"
     "dirtree"
     "fontenc"
@@ -38,7 +39,8 @@
     "fig:algoDDPG"
     "fig:randompolicy"
     "fig:benchmark"
-    "subsubsec:hp_params")
+    "subsubsec:hp_params"
+    "tab:hyperparams")
    (LaTeX-add-bibliographies)
    (LaTeX-add-xcolor-definecolors
     "codegreen"
